3fc50ab3a29671eb1493527f1b03be638eebc701
SirGrimm254 <alexandergathendo666@gmail.com>
2024-08-13 20:09:36 +0300

    update website files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const mangaList = [
     { id: 1, title: 'Nano Machine', category: 'Action', imageUrl: '/images/manga1.jpg' },
     { id: 2, title: 'Solo Levelling', category: 'Fantasy', imageUrl: '/images/manga2.jpg' },
-    { id: 3, title: 'Demonic Emperor', category: 'Horror', imageUrl: '/images/manga3.jpg' },
+    { id: 3, title: 'Magic Emperor Emperor', category: 'Horror', imageUrl: '/images/manga3.jpg' },
     { id: 4, title: "Heavenly Demon Can't Live A Normal Life", category: 'Martial Arts', imageUrl: '/images/manga4.jpg' },
     { id: 5, title: 'Solo Levelling', category: 'Action', imageUrl: '/images/manga2.jpg' },
     { id: 6, title: 'Magic Emperor', category: 'Action', imageUrl: '/images/manga3.jpg' },
