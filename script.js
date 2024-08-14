a507d49e2daaf7799e87222d481b0a9a4a0f83ec
SirGrimm254 <alexandergathendo666@gmail.com>
2024-08-14 05:42:56 +0300

    update

--- script.js
+++ script.js
@@ -15,51 +15,22 @@ function fetchMangaByCategory(category) {
     .then(response => response.json())
     .then(data => {
       const mangaList = document.getElementById('manga-list');
-      mangaList.innerHTML = '';
       if (data.length === 0) {
         mangaList.innerHTML = `<p>No manga found for the category "${category}".</p>`;
         return;
-    }
-      const ul = document.createElement('ul');
-
-      data.forEach((manga) => {
-        const li = document.createElement('li');
+    } else {
+        const ul = document.createElement('ul');
+        data.forEach((manga) => {
+            const li = document.createElement('li');
         li.innerHTML = `
           <img src="${manga.imageUrl}" alt="${manga.title}" class="manga-icon">
           <a href="${manga.detailPage}" class="manga-title">${manga.title}</a>
           <ul></ul>
         `;
         ul.appendChild(li);
-      
-        // Generate sub-manga list
-        if (manga.subManga) {
-          const subUl = li.querySelector('ul');
-          manga.subManga.forEach((subManga) => {
-            const subLi = document.createElement('li');
-            subLi.innerHTML = `
-              <img src="${subManga.imageUrl}" alt="${subManga.title}" class="manga-icon">
-              <a href="${subManga.detailPage}" class="manga-title">${subManga.title}</a>
-              <ul></ul>
-            `;
-            subUl.appendChild(subLi);
-
-            // Generate sub-sub-manga list
-            if (subManga.subManga) {
-              const subSubUl = subLi.querySelector('ul');
-              subManga.subManga.forEach((subSubManga) => {
-                const subSubLi = document.createElement('li');
-                subSubLi.innerHTML = `
-                  <img src="${subSubManga.imageUrl}" alt="${subSubManga.title}" class="manga-icon">
-                  <a href="${subSubManga.detailPage}" class="manga-title">${subSubManga.title}</a>
-                `;
-                subSubUl.appendChild(subSubLi);
-              });
-            }
-          });
-        }
-      });
-
-      mangaList.appendChild(ul);
-    })
-    .catch(error => console.error('Error:', error));
+        });
+        mangaList.innerHTML = '';
+        mangaList.appendChild(ul);
+      }
+    });
 }
