5c1cc611644bdb42a3db0f82bb661c3402efd5c5
SirGrimm254 <alexandergathendo666@gmail.com>
2024-08-15 10:18:52 +0300

    update

--- pages/api/manga.js
+++ pages/api/manga.js
@@ -2,8 +2,9 @@ export default function handler(req, res) {
     // Sample manga data
     const mangaList = [
         { id: 1, title: 'Nano Machine', genre: 'Action' },
-        { id: 2, title: 'Sol Levelling', genre: 'Fantasy' },
-        { id: 3, title: 'Magic Emperor', genre: 'Horror' }
+        { id: 2, title: 'Solo Levelling', genre: 'Fantasy' },
+        { id: 3, title: 'Magic Emperor', genre: 'Horror' },
+        { id: 4, title: 'Heavenly Demon Can\'t Live A Normal Life', genre: 'Martial Arts' }
     ];
 
     // Respond with the manga list as JSON
